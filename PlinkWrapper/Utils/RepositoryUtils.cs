2e63a4223d58d174f8ca46f6257b6ded6c9248c9
Ievgenii Iukhymchuk <ievgenii_iukhymchuk@epam.com>
2019-04-04 19:57:46 +0300

    Use default settings ppk for unknown github account.

--- PlinkWrapper/Utils/RepositoryUtils.cs
+++ PlinkWrapper/Utils/RepositoryUtils.cs
@@ -4,15 +4,25 @@ namespace PlinkWrapper.Utils
 {
     static class RepositoryUtils
     {
-       internal static string GetPuttySessionKeyPath(string sessionName)
-       {
-           if (string.IsNullOrEmpty(sessionName)) return null;
-           
-           var sessionPath = $@"Software\SimonTatham\PuTTY\Sessions\{sessionName}";
-           using (var session = Registry.CurrentUser.OpenSubKey(sessionPath))
-           {
-               return session?.GetValue("PublicKeyFile") as string;
-           }
-       }
-   }
+        const string publicKey = "PublicKeyFile";
+        const string defaultSessionName = "Default%20Settings";
+        const string puttySessionsregistryPath = @"Software\SimonTatham\PuTTY\Sessions\";
+
+        internal static string GetPuttySessionKeyPath(string sessionName)
+        {
+            if (string.IsNullOrEmpty(sessionName)) return null;
+
+            var sessionPath = puttySessionsregistryPath + sessionName;
+            return GetValue(puttySessionsregistryPath + sessionName, publicKey)
+                ?? GetValue(puttySessionsregistryPath + defaultSessionName, publicKey);
+        }
+
+        static string GetValue(string registryPath, string value)
+        {
+            using (var session = Registry.CurrentUser.OpenSubKey(registryPath))
+            {
+                return session?.GetValue(value) as string;
+            }
+        }
+    }
 }
